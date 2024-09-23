f6455551572da4ec9975bad0f4754df21da9263b
mos03351 <mos03351@gmail.com>
2024-09-23 13:04:21 +0700

    update

--- script.js
+++ script.js
@@ -6,3 +6,33 @@ window.addEventListener('scroll', function() {
         profile.classList.remove('shrink'); // ลบคลาสเมื่อกลับขึ้น
     }
 });
+function showLoading() {
+    document.getElementById('loading').style.display = 'flex';
+}
+
+function hideLoading() {
+    document.getElementById('loading').style.display = 'none';
+}
+
+function sellWaste(event) {
+    event.preventDefault();
+    showLoading();
+    
+    // จำลองการเรียก Ajax
+    setTimeout(() => {
+        // ซ่อน loading หลังจากโหลดเสร็จ
+        hideLoading();
+        alert('ขายขยะ'); // แทนที่ด้วยการเรียก API ของคุณ
+    }, 2000); // หน่วงเวลา 2 วินาที
+}
+
+function donateWaste(event) {
+    event.preventDefault();
+    showLoading();
+    
+    // จำลองการเรียก Ajax
+    setTimeout(() => {
+        hideLoading();
+        alert('ซื้อขยะ'); // แทนที่ด้วยการเรียก API ของคุณ
+    }, 2000); // หน่วงเวลา 2 วินาที
+}
